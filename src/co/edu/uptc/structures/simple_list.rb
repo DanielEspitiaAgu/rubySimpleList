f4e3ed3fe7b3be1ef3d685e9bb90aac803e58dfb
DanielEspitiaAgu <daniel.espitia01@uptc.edu.co>
2025-09-06 11:45:33 -0500

    Definicion de metodos

---
+++ src/co/edu/uptc/structures/simple_list.rb
@@ -0,0 +1,60 @@
+class SimpleList
+
+  def initialize
+    @head = nil
+  end
+
+  def add(element)
+  end
+
+  def clear
+  end
+
+  def size
+  end
+
+  def empty?
+  end
+
+  def contains?(obj)
+  end
+
+  def each
+  end
+
+  def to_a
+  end
+
+  def remove(obj)
+  end
+
+  def get(index)
+  end
+
+  def set(index, element)
+  end
+
+  def add_at(index, element)
+  end
+
+  def remove_at(index)
+  end
+
+  def index_of(obj)
+  end
+
+  def last_index_of(obj)
+  end
+
+  def sub_list(from_index, to_index)
+  end
+
+  def to_s
+  end
+
+  private
+
+  def node_at(index)
+  end
+  
+end
