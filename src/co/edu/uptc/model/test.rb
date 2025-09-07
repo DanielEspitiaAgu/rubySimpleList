cc2dec9566f4d2ea50d1b4467f41cc2710091b5e
DanielEspitiaAgu <daniel.espitia01@uptc.edu.co>
2025-09-06 19:43:23 -0500

    Completing methods and corrections (size, isEmpty.)

--- src/co/edu/uptc/model/test.rb
+++ src/co/edu/uptc/model/test.rb
@@ -4,19 +4,41 @@ require_relative '../structures/node'
 class Test
   def self.run
     my_list = SimpleList.new
+
+    puts "Esta vacia"
+    puts my_list.empty?
+
     my_list.add(1)
     my_list.add(2)
     my_list.add(3)
     my_list.add(4)
+    my_list.add(5)
 
     puts my_list.get(0)
     puts my_list.get(1)
     puts my_list.get(2)
     puts my_list.get(3)
 
-    my_list.remove_at(0)
+    puts "Esta es la lista despues de remove_add" 
 
-    puts my_list.get(0)
+    puts my_list.remove_at(0)
+
+    puts my_list.to_s();
+
+    my_list.each do |value|
+      puts "Numero: #{value}"
+    end
+
+    puts "Esta es la lista despues de add_at" 
+
+    my_list.add_at(0,3)
+    my_list.add_at(1,4)
+    my_list.add_at(2,5)
+
+    puts my_list.to_s();
+
+    my_list.set(0, 1);
+
   end
 end
 
--- src/co/edu/uptc/structures/simple_list.rb
+++ src/co/edu/uptc/structures/simple_list.rb
@@ -1,6 +1,5 @@
-
-
 class SimpleList
+   include Enumerable
 
   def initialize
     @head = nil
@@ -23,18 +22,44 @@ class SimpleList
   end
 
   def size  
+    count = 0
+    current = @head
+    while current
+      count += 1
+      current = current.next_node
+    end
+    count
   end
 
   def empty?
+    @head.nil?
   end
 
   def contains?(obj)
+    current = @head
+    while current
+      return true if current.value == obj
+      current = current.next_node
+    end
+    false
   end
 
   def each
+    current = @head
+    while current
+      yield current.value
+      current = current.next_node
+    end
   end
 
   def to_a
+    array = []
+    current = @head
+    while current
+      array << current.value
+      current = current.next_node
+    end
+  array
   end
 
   def remove?(obj)
@@ -63,22 +88,6 @@ class SimpleList
     return get_node_at(index).value
   end
 
-  private
-
-  def get_node_at(index)
-    current=@head
-    current_index=0
-    while current!=nil
-      if current_index == index
-        return current    
-      end
-      current = current.next_node
-      current_index+=1
-    end
-    raise "índice fuera de rango"
-    
-  end
-
   def set(index, element)
     validate_set_exceptions(index, element)
     uptdate_node= get_node_at(index)
@@ -86,19 +95,7 @@ class SimpleList
     uptdate_node.value = element
     return old_value 
   end
-
-  private
-
-   def validate_set_exceptions(index, element)
-    if index<0
-      raise IndexError, "Índice negativo"
-    end
-    if element.nil?
-      raise "El elemento es nulo"  
-   
-    end
-   end
-   
+  
   def add_at(index, element)
     if element.nil?
       raise "Elemento nulo"  
@@ -106,7 +103,7 @@ class SimpleList
     end
     if index<0 || index> size
       raise "Indice fuera de rango" 
-          
+      
     end
     if index == 0
       new_node = Node.new(element)
@@ -115,52 +112,68 @@ class SimpleList
       
     else
       current = @head
-#Arreglar
-     i=0
-     while i < index-1
+      #Arreglar
+      i=0
+      while i < index-1
         break if current.next_node.nil?
         current = current.next_node
         i+=1
       end
-       new_node = new Node (element)
+      new_node = Node.new(element)
       new_node.next_node = current.next_node
       current.next_node = new_node
     end
   end
-
+  
   def remove_at(index)
     if index <0
       raise "Indice fuera del rango"     
-    
+      
     end
     if index==0 
       removed_element = @head.value
       @head = @head.next_node
       return removed_element
-    
+      
     end
     current= @head.next_node
     previous = @head
-#Arreglar 
-   i = 0
-while i < index - 1
-  current = current.next_node
-  previous = previous.next_node
-  i += 1
-end
-
-
+    #Arreglar 
+    i = 0
+    while i < index - 1
+      current = current.next_node
+      previous = previous.next_node
+      i += 1
+    end
+    
     removed_element = current.value
     previous.next_node = current.next_node
     return removed_element
   end
-
+  
   def index_of(obj)
+    current = @head
+    index = 0
+    while current
+      return index if current.value == obj
+      current = current.next_node
+      index += 1
+    end
+    -1
   end
-
+  
   def last_index_of(obj)
+    current = @head
+    index = 0
+    last_index = -1
+    while current
+      last_index = index if current.value == obj
+      current = current.next_node
+    index += 1
+    end
+    last_index
   end
-
+  
   def sub_list(from_index, to_index)
     sublist = SimpleList.new
     if from_index<0 || to_index > size || from_index > to_index
@@ -179,7 +192,7 @@ end
     end
     return sublist
   end
-
+  
   def to_s
   elements = []
   current = @head
@@ -188,11 +201,51 @@ end
     current = current.next_node
   end
   "Lista simple: [#{elements.join(", ")}]"
-end
+  end
+
+  private
+  
+  def get_node_at(index)
+    current=@head
+    current_index=0
+    while current!=nil
+      if current_index == index
+        return current    
+      end
+      current = current.next_node
+      current_index+=1
+    end
+    raise "índice fuera de rango"
+    
+  end
 
   private
 
+  def validate_set_exceptions(index, element)
+    if index<0
+      raise IndexError, "Índice negativo"
+    end
+    if element.nil?
+      raise "El elemento es nulo"  
+      
+    end
+  end
+  
+  private
+
   def node_at(index)
+    raise "Índice negativo" if index < 0
+
+    current = @head
+    current_index = 0
+
+    while current
+      return current if current_index == index
+      current = current.next_node
+      current_index += 1
+    end
+
+    raise "Índice fuera de rango"
   end
   
 end
