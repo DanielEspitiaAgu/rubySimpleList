1c6933426c6b26382f712f8de91f4fac692622dc
DanielEspitiaAgu <daniel.espitia01@uptc.edu.co>
2025-09-09 20:45:50 -0500

    Algunos metodos fueron eliminados, Test Actualizado, Carpeta vs-code quitada para evitar conflictos Co-authored-by: SantiagoHurtadoNieto <SantiagoHurtadoNieto@users.noreply.github.com>

--- src/co/edu/uptc/model/test.rb
+++ src/co/edu/uptc/model/test.rb
@@ -3,49 +3,114 @@ require_relative '../structures/node'
 
 class Test
   def self.run
-    my_list = SimpleList.new
+    list = SimpleList.new
 
-    puts "Esta vacia"
-    puts my_list.empty?
-
-    my_list.add(1)
-    my_list.add(2)
-    my_list.add(3)
-    my_list.add(4)
-    my_list.add(5)
-
-    puts my_list.get(0)
-    puts my_list.get(1)
-    puts my_list.get(2)
-    puts my_list.get(3)
-
-    puts "Esta es la lista despues de remove_add" 
-
-    puts my_list.remove_at(0)
-
-    puts my_list.to_s();
-
-    my_list.each do |value|
-      puts "Numero: #{value}"
+    loop do
+      show_menu
+      option = gets.chomp.to_i
+      case option
+      when 1
+        add_element(list)
+      when 2
+        remove_element(list)
+      when 3
+        check_empty(list)
+      when 4
+        show_list(list)
+      when 5
+        object_position(list)
+      when 6
+        object_at_position(list)
+      when 7
+        object_count(list)
+      when 8
+        contains_object(list)
+      when 9
+        show_each(list)
+      when 0
+        puts "¡Hasta luego!"
+        break
+      else
+        puts "Opción no válida."
+      end
     end
+  end
 
-    puts "Esta es la lista despues de add_at" 
+  def self.show_menu
+    puts "\n--- Menú ---"
+    puts "1. Agregar elemento"
+    puts "2. Eliminar elemento"
+    puts "3. Verificar que la lista está vacía"
+    puts "4. ToString"
+    puts "5. Posición de un objeto"
+    puts "6. Objeto en la posición"
+    puts "7. Cantidad de objetos en la lista"
+    puts "8. ¿Se contiene un objeto en la lista?"
+    puts "9. Mostrar objetos con each"
+    puts "0. Salir"
+    print "Seleccione una opción: "
+  end
 
-    my_list.add_at(0,3)
-    my_list.add_at(1,4)
-    my_list.add_at(2,5)
+  def self.add_element(list)
+    print "Ingrese el elemento a agregar: "
+    value = gets.chomp
+    list.add(value)
+    puts "Elemento agregado."
+  end
 
-    puts my_list.to_s();
+  def self.remove_element(list)
+    print "Ingrese el elemento a eliminar: "
+    value = gets.chomp
+    if list.remove?(value)
+      puts "Elemento eliminado."
+    else
+      puts "Elemento no encontrado."
+    end
+  end
 
-    my_list.set(0, 1);
-    
-    my_list.add("Camila")
-    
-    puts my_list.to_s
+  def self.check_empty(list)
+    puts list.empty? ? "La lista está vacía." : "La lista NO está vacía."
+  end
 
-    
+  def self.show_list(list)
+    puts list.to_s
+  end
 
+  def self.object_position(list)
+    print "Ingrese el objeto a buscar: "
+    value = gets.chomp
+    pos = list.index_of(value)
+    if pos == -1
+      puts "El objeto no está en la lista."
+    else
+      puts "La posición del objeto es: #{pos}"
+    end
+  end
+
+  def self.object_at_position(list)
+    print "Ingrese la posición: "
+    idx = gets.chomp.to_i
+    begin
+      puts "El objeto en la posición #{idx} es: #{list.get(idx)}"
+    rescue
+      puts "Índice fuera de rango."
+    end
+  end
+
+  def self.object_count(list)
+    puts "Cantidad de objetos en la lista: #{list.size}"
+  end
+
+  def self.contains_object(list)
+    print "Ingrese el objeto a verificar: "
+    value = gets.chomp
+    puts list.contains?(value) ? "El objeto está en la lista." : "El objeto NO está en la lista."
+  end
+
+  def self.show_each(list)
+    puts "Objetos en la lista:"
+    list.each { |value| puts value }
   end
 end
 
-Test.run 
+Test.run
--- src/co/edu/uptc/structures/simple_list.rb
+++ src/co/edu/uptc/structures/simple_list.rb
@@ -1,5 +1,4 @@
 class SimpleList
-   include Enumerable
 
   def initialize
     @head = nil
@@ -17,9 +16,6 @@ class SimpleList
     true
   end
 
-  def clear
-    @head= nil
-  end
 
   def size  
     count = 0
@@ -52,16 +48,6 @@ class SimpleList
     end
   end
 
-  def to_a
-    array = []
-    current = @head
-    while current
-      array << current.value
-      current = current.next_node
-    end
-  array
-  end
-
   def remove?(obj)
     current = @head
     previous = nil
@@ -85,70 +71,14 @@ class SimpleList
   end
 
   def get(index)
-    return get_node_at(index).value
-  end
-
-  def set(index, element)
-    validate_set_exceptions(index, element)
-    uptdate_node= get_node_at(index)
-    old_value = uptdate_node.value
-    uptdate_node.value = element
-    return old_value 
-  end
-  
-  def add_at(index, element)
-    if element.nil?
-      raise "Elemento nulo"  
-
-    end
-    if index<0 || index> size
-      raise "Indice fuera de rango" 
-      
-    end
-    if index == 0
-      new_node = Node.new(element)
-      new_node.next_node= @head
-      @head = new_node
-      
-    else
-      current = @head
-      
-      i=0
-      while i < index-1
-        break if current.next_node.nil?
-        current = current.next_node
-        i+=1
-      end
-      new_node = Node.new(element)
-      new_node.next_node = current.next_node
-      current.next_node = new_node
-    end
-  end
-  
-  def remove_at(index)
-    if index <0
-      raise "Index out of bounds"     
-      
-    end
-    if index==0 
-      removed_element = @head.value
-      @head = @head.next_node
-      return removed_element
-      
-    end
-    current= @head.next_node
-    previous = @head
-     
-    i = 0
-    while i < index - 1
+    current = @head
+    current_index = 0
+    while current != nil
+      return current.value if current_index == index
       current = current.next_node
-      previous = previous.next_node
-      i += 1
+      current_index += 1
     end
-    
-    removed_element = current.value
-    previous.next_node = current.next_node
-    return removed_element
+    raise "Index out of bounds"
   end
   
   def index_of(obj)
@@ -162,37 +92,6 @@ class SimpleList
     -1
   end
   
-  def last_index_of(obj)
-    current = @head
-    index = 0
-    last_index = -1
-    while current
-      last_index = index if current.value == obj
-      current = current.next_node
-    index += 1
-    end
-    last_index
-  end
-  
-  def sub_list(from_index, to_index)
-    sublist = SimpleList.new
-    if from_index<0 || to_index > size || from_index > to_index
-      raise "Index out of bounds"
-    end
-    current = @head
-    index =0
-    while current!= nil && index < from_index
-      current = current.next_node
-      index+=1
-    end
-    while current!= nil && index <= to_index
-      sublist.add(current.value)
-      current = current.next_node
-      index +=1
-    end
-    return sublist
-  end
-  
   def to_s
   elements = []
   current = @head
@@ -201,51 +100,6 @@ class SimpleList
     current = current.next_node
   end
   "Lista simple: [#{elements.join(", ")}]"
-  end
-
-  private
-  
-  def get_node_at(index)
-    current=@head
-    current_index=0
-    while current!=nil
-      if current_index == index
-        return current    
-      end
-      current = current.next_node
-      current_index+=1
-    end
-    raise "Index out of bounds"
-    
-  end
-
-  private
-
-  def validate_set_exceptions(index, element)
-    if index<0
-      raise IndexError, "Index out of bounds"
-    end
-    if element.nil?
-      raise "El elemento es nulo"  
-      
-    end
-  end
-  
-  private
-
-  def node_at(index)
-    raise "Negative index" if index < 0
-
-    current = @head
-    current_index = 0
-
-    while current
-      return current if current_index == index
-      current = current.next_node
-      current_index += 1
-    end
-
-    raise "Index out of bounds"
   end
   
 end
